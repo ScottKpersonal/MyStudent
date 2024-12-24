09da9fccb2a7437a8c399f0f73b0fcf058d390fc
Scott K <kredoscott@hotmail.com>
2024-12-24 09:49:54 +0200

    Adding the UniversityClass class and updating the name of the user in App

---
+++ src/UniversityClass.java
@@ -0,0 +1,71 @@
+public class UniversityClass {
+    // Instance variables
+    private String className;
+    private String classId;
+    private int numberOfSemesters;
+    private String teacher;
+    private String faculty;
+
+    // Constructor
+    public UniversityClass(String className, String classId, int numberOfSemesters, 
+                          String teacher, String faculty) {
+        this.className = className;
+        this.classId = classId;
+        this.numberOfSemesters = numberOfSemesters;
+        this.teacher = teacher;
+        this.faculty = faculty;
+    }
+
+    // Getters and Setters
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+
+    public int getNumberOfSemesters() {
+        return numberOfSemesters;
+    }
+
+    public void setNumberOfSemesters(int numberOfSemesters) {
+        this.numberOfSemesters = numberOfSemesters;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    // toString method for easy printing of class information
+    @Override
+    public String toString() {
+        return "UniversityClass{" +
+                "className='" + className + '\'' +
+                ", classId='" + classId + '\'' +
+                ", numberOfSemesters=" + numberOfSemesters +
+                ", teacher='" + teacher + '\'' +
+                ", faculty='" + faculty + '\'' +
+                '}';
+    }
+}
